f04ee612e1ea6aa3e64210ecc752e601a05d339a
Andrei-dev27 <newpage289@outlook.com>
2021-05-27 00:36:29 +0300

    uppercase first character

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,17 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length == 0) {
+  return str;
+  }
+  else {
+    str.toLowerCase();
+    return str[0].toUpperCase() + str.slice(1);
+  }
 }
+
+ucFirst(str)
+
+
+
+
+
+
