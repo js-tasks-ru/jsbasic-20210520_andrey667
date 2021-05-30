1b1b95941409420578027b3d68e32554aa6b652e
Andrei-dev27 <newpage289@outlook.com>
2021-05-31 00:10:04 +0300

    2-module-2-task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,22 @@
 function isEmpty(obj) {
-  // ваш код...
-}
+  for (const k in obj) {
+    return false;
+  }
+  return true;
+} 
+
+ 
+
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); 
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); 
+
+
+
+
+
