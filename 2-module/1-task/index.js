cf3d667fcf14c01ffcde2d62f0b6471bfc4b647e
Andrei-dev27 <newpage289@outlook.com>
2021-05-30 22:14:32 +0300

    2-module-1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,24 @@
-function sumSalary(salaries) {
-  // ваш код...
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
 }
+
+
+
+
+function sumSalary(Obj) {
+  let total = 0;
+  for (const k in Obj) {
+      if(typeof Obj[k] === "number" && isFinite (Obj[k]) ) {
+        total += Obj[k];
+      }
+  }
+  return total;
+}
+
+sumSalary(salaries)
+
