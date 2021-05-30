aba863e9ee24d773befa1657031ae57651aa03cd
Andrei-dev27 <newpage289@outlook.com>
2021-05-31 02:37:15 +0300

    2-module-3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,24 @@
 let calculator = {
-  // ваш код
+  read: function(a, b) {
+    calculator.firstValue = a;
+    calculator.secondValue = b;
+  },
+  sum: function() {
+    let sumFirstValue = calculator.firstValue;
+    let sumSecondValue = calculator.secondValue;
+    return sumFirstValue + sumSecondValue;
+  },
+  mul: function() {
+    let mulFirstValue = calculator.firstValue;
+    let mulSecondValue = calculator.secondValue;
+    return mulFirstValue * mulSecondValue;
+  }
 };
+
+calculator.read(3, 5);
+console.log(calculator);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul()); // 15
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
