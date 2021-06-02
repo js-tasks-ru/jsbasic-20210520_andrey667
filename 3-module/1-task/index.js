67af0945ed39dc2eeb0bd8465ff83a0aa43b2a94
Andrei-dev27 <newpage289@outlook.com>
2021-06-02 14:53:21 +0300

    3-module

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,15 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+
 function namify(users) {
-  // ваш код...
-}
+  const result = users.map((user) => {
+    return `${user.name}`;
+  });
+    return result;
+};
+
+let names = namify(users); // ['Вася', 'Петя', 'Маша']
