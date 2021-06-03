e6769b0ebc29e6ed63dde4fd84578b1cb7229728
Andrei-dev27 <newpage289@outlook.com>
2021-06-03 13:29:50 +0300

    5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str
+  .split(',')
+  .join(" ")
+  .split(" ")
+  .map(numb => Number(numb))
+  .filter(filter => typeof(filter) === "number" && isFinite (filter));
+  const Obj = {
+    min: Math.min.apply(null, arr),
+    max: Math.max.apply(null, arr),
+  }
+  return Obj;
 }
