266909096397bb384d89e9a0a7f550f0695531dd
Andrei-dev27 <newpage289@outlook.com>
2021-06-08 15:11:28 +0300

    4-module-3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,28 @@
 function highlight(table) {
-  // ваш код...
+  let rows = table.querySelectorAll('tr');
+
+  for (let tr of rows) {
+    for (let td of tr.children) {
+      
+      if (td.dataset.available === "true")  {
+        tr.classList.add('available')
+      } else if (td.dataset.available === "false") {
+        tr.classList.add('unavailable')
+      } else if (!td.dataset.available) {
+        td.parentElement.hidden = true
+      }
+
+      if (td.innerHTML === "m") {
+        tr.classList.add('male')
+      } else if (td.innerHTML === "f") {
+        tr.classList.add('female')
+      }
+
+      if (td.innerHTML < 18) {
+        tr.style = "text-decoration: line-through"
+      }
+
+    }
+  }
+
 }
